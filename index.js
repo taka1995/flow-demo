f2c37987f1328a9e0905a9c07e06d03cf6131c3e
taka1995 <38433955+taka1995@users.noreply.github.com>
2019-10-10 14:31:17 +0900

    Update index.js

--- index.js
+++ index.js
@@ -25,3 +25,16 @@ app.listen(app.get('port'), function() {
 });
 
 module.exports = app
+
+const cool = require('cool-ascii-faces')
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .get('/cool', (req, res) => res.send(cool()))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
